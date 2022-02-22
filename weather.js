0dc5ecfc4d62b0f6e437e0b7eaf32228231e43ff
Glitch (glitch-hello-website) <none>
2022-02-22 08:24:13 +0000

    🚖👹 Checkpoint ./weather.js:37591749/3

--- weather.js
+++ weather.js
@@ -1,11 +1,11 @@
-var button = document.querySelector('.button')
-var inputValue = document.querySelector('.inputValue')
+var button = document.querySelector('.button');
+var inputValue = document.querySelector('.inputValue');
 var cityName =document.querySelector('.name');
 var desc =document.querySelector('.desc');
 var temp =document.querySelector('.temp');
 
 button.addEventListener('click', function(){
-fetch('http://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=f922718fb7a613d97b21653c8649e105')
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=f922718fb7a613d97b21653c8649e105')
  .then(response => response.json())
  .then(data => {
      var nameValue =data['name'];
