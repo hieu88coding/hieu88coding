062e31581f5ad4b673854c308f6c7209753bc9d3
Glitch (glitch-hello-website) <none>
2022-02-22 08:12:14 +0000

    🐌🌞 Checkpoint ./index.html:37591749/639 ./weather.js:37591749/736

---
+++ weather.js
@@ -0,0 +1,21 @@
+var button = document.querySelector('.button')
+var inputValue = document.querySelector('.inputValue')
+var cityName =document.querySelector('.name');
+var desc =document.querySelector('.desc');
+var temp =document.querySelector('.temp');
+
+button.addEventListener('click', function(){
+fetch('http://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=f922718fb7a613d97b21653c8649e105')
+ .then(response => response.json())
+ .then(data => {
+     var nameValue =data['name'];
+     var tempValue = data['main']['temp'];
+     var descValue = data['weather'][0]['description'];
+
+     cityName.innerHTML =nameValue;
+     temp.innerHTML =tempValue;
+     desc.innerHTML =descValue;
+ })
+
+ .catch(err => alert("Wrong City Name!"))
+})
