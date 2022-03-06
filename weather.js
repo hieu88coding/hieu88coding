0e0b02e6dad62de202760affb77ea78e66057c8d
Glitch (glitch-hello-website) <none>
2022-03-06 15:12:45 +0000

    🍣🎁 Checkpoint ./index.html:37591749/109 ./weather.js:37591749/5

--- weather.js
+++ weather.js
@@ -39,21 +39,21 @@ function weatherDetails(info){
     
     if(id == 800){
             wIcon.src = "icons/clear.svg";
-            add_Child("Trời quang mây tạnh")
+            add_Child("Trời quang mây tạnh.")
         }else if(id >= 200 && id <= 232){
             wIcon.src = "icons/storm.svg";
-            add_Child("Mưa gió bão bùng")
+            add_Child("Mưa gió bão bùng.")
         }else if(id >= 600 && id <= 622){
             wIcon.src = "icons/snow.svg";
         }else if(id >= 701 && id <= 781){
             wIcon.src = "icons/haze.svg";
-          add_Child("Khói tỏa mùa sương")
+          add_Child("Khói tỏa mùa sương.")
         }else if(id >= 801 && id <= 804){
             wIcon.src = "icons/cloud.svg";
-            add_Child("Mây mer cùng hater")
+            add_Child("Mây mer cùng hater.")
         }else if((id >= 500 && id <= 531) || (id >= 300 && id <= 321)){
             wIcon.src = "icons/rain.svg";
-            add_Child("Mưa lạnh")
+            add_Child("Mưa lạnh.")
             
         }
     
